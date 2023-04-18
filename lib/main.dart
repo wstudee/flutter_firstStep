d1269929b1afb60c12c6846e84ce07962e7feabf
wstudee <wstudlee@gmail.com>
2023-04-18 23:35:48 +0900

    layout homework

--- lib/main.dart
+++ lib/main.dart
@@ -15,71 +15,71 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Color(0xffffffff),
-          title: Text('금호동 3가', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blue)),
-          actions: [Icon(Icons.search), Icon(Icons.menu), Icon(Icons.notifications_none)],
+          title: Text('봉산동', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black)),
+          actions: [
+            Icon(Icons.search, color: Colors.black),
+            Icon(Icons.menu, color: Colors.black),
+            Icon(Icons.notifications_none, color: Colors.black)
+          ],
         ),
         body: SizedBox(
           child: Container(
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.red),
-            ),
-            width: double.infinity,
-            padding: EdgeInsets.all(5),
-            child: Container(
-              width: double.infinity,
-              height: 200,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Expanded(
-                    flex: 3,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.orange),
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(border: Border.all(color: Colors.red)),
+            child: Row(
+              children: [
+                Container(
+                  child: Image(image: AssetImage('../assets/shuri.png')),
+                  decoration: BoxDecoration(border: Border.all(color: Colors.orange)),
+                  width: 120,
+                  height: 120,
+                  margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                ),
+                Container(
+                  height: 120,
+                  child: Column(
+                    children: [
+                      Container(
+                        child: Text('귀야호 슈리~~~~ (뽕주댕이 2짝, 너구리꼬리 1개 포함)'),
+                        decoration: BoxDecoration(border: Border.all(color: Colors.yellow)),
+                        height: 40,
+                        width: 300,
                       ),
-                    ),
+                      Container(
+                        child: Text('경기도 평택시 연쥐하우스·끌올 10분 전',
+                          style: TextStyle(color: Color(0xffc9c3c3), fontSize: 12),),
+                        decoration: BoxDecoration(border: Border.all(color: Colors.yellow)),
+                        height: 25,
+                        width: 300,
+                      ),
+                      Container(
+                        child: Text('486원', style: TextStyle(fontWeight: FontWeight.bold ),),
+                        decoration: BoxDecoration(border: Border.all(color: Colors.yellow)),
+                        height: 25,
+                        width: 300,
+                      ),
+                      Container(
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Icon(Icons.favorite_border),
+                                Text('486')
+                              ],
+                            )
+                          ],
+                        ),
+                        decoration: BoxDecoration(border: Border.all(color: Colors.yellow)),
+                        height: 30,
+                        width: 300,
+                      ),
+                    ],
                   ),
-                  Expanded(
-                    flex: 7,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.green),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.purple),
-                            ),
-
-                          ),
-                          Container(
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.purple),
-                            ),
-
-                          ),
-                          Container(
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.purple),
-                            ),
-
-                          ),
-                          Container(
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.purple),
-                            ),
-
-                          )
-                        ],
-                      ),
-                    ),
-                  )
-                ],
-              ),
+                )
+              ],
             ),
-            )
+          )
           )
         )
       );
